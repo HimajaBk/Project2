b0f3edd2bb0ebe10f9dfd72ef7e94f3f6c460cb7
Hima <himaja.bk@gmail.com>
2017-02-03 08:40:08 -0600

    Selenium Example

---
+++ seleniumEx/LocatorsEx1.java
@@ -0,0 +1,18 @@
+package seleniumEx;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class LocatorsEx1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		WebDriver obj1 = new FirefoxDriver();
+		obj1.get("http://qaclickacademy.usefedora.com/");
+		obj1.findElement(By.xpath("html/body/div[1]/center/a")).click();
+
+	}
+
+}
